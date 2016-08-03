30d867c4625de5d0c964142d8b0144143f410dbc
Raphael 'kena' Poss <knz@cockroachlabs.com>
2016-08-03 17:49:51 +0000

    Improve the index page.

--- report.py
+++ report.py
@@ -25,6 +25,11 @@ def header(user):
    td { font-family: monospace !important; }
    .hr { height: 3px; color: solid black; }
    </style>
+   <script>
+   $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+   });
+   </script>
    <h1>Issue dashboard for %s</h1>
    """ % (user,user))
    print("<p>Generated on {0} UTC</p>".format(time.asctime(time.gmtime())))
@@ -74,13 +79,14 @@ def genSection(title, divid, query, colorize):
        <td><span class='hidden'>{updated}</span>{hupdated}</td>
        <td>{milestone}</td>
    </tr>""".format(**locals()))
-   
+
    print("""</tbody></table>""")
    print("""
       </div>
      </div>
     </div>
     """)
+   return nrows
 
 def newIsBad(meta):
     old = meta['created'] <= datetime.now()-timedelta(weeks=1)
@@ -94,20 +100,10 @@ def oldIsBad(meta):
         return 'warning'
     return ''
 
-index = open('dashboard/index.html', 'w')
-sys.stdout = index
-header('all')
-print("<table class='table table-condensed table-striped table-hover table-responsive' style='width:auto'><thead>")
-print("<tr><th>Username</th><th>Dashboard link</th><th>Github profile</th></tr></thead><tbody>")
-
-for row in cu.execute("select name, lower(name) as lname from users where crl=1 order by lname"):
-    user = row[0]
-    luser = row[1]
-    sys.stdout = index
-    print("<tr><td>%s</td><td><a href='%s.html'>dashboard</a></td><td><a href='https://github.com/%s'>profile</a></td></tr>" % (luser, user, user))
-    sys.stdout = open('dashboard/' + user + '.html', 'w')
-    header(user)
-    genSection("Issues from external users without milestone nor assignment", "extIssues", """
+ops = {
+   "extIssues" : {
+      'title':  "Issues from external users without milestone nor assignment",
+      'query':  """
      select issues.url, issues.number, issues.title, issues.created, reported.user, issues.assignee, issues.updated, issues.milestone
        from issues 
             join reported on reported.issue=issues.number 
@@ -115,35 +111,73 @@ for row in cu.execute("select name, lower(name) as lname from users where crl=1 
       where not exists(select * from assigned where issue=issues.number) 
         and issues.milestone=""
    order by issues.category,issues.updated
-   """, oldIsBad)
-
-    genSection("Issues assigned to you", "yourIssues", """
+      """},
+   "yourIssues":{
+      'title': "Issues assigned to you",
+      'query': """
      select issues.url, issues.number, issues.title, issues.created, reported.user, issues.assignee, issues.updated, issues.milestone
        from issues 
             join reported on reported.issue=issues.number 
             join assigned on assigned.issue=issues.number and assigned.user='{0}'
    order by issues.category,issues.updated
-    """.format(user), oldIsBad)
-
-    genSection("Issues created by you that nobody is working on", "unIssues", """
+      """},
+   "unIssues":{
+      'title':"Issues created by you that nobody is working on",
+      'query': """
      select issues.url, issues.number, issues.title, issues.created, reported.user, issues.assignee, issues.updated, issues.milestone
        from issues 
             join reported on reported.issue=issues.number and reported.user='{0}'
       where not exists (select * from assigned where issue=issues.number)
    order by issues.category,issues.updated
-    """.format(user), oldIsBad)
-
-    genSection("Issues you created that someone else is working on", "childIssues", """
+      """},
+   "childIssues":{
+      'title': "Issues you created that someone else is working on",
+      'query': """
      select issues.url, issues.number, issues.title, issues.created, reported.user, issues.assignee, issues.updated, issues.milestone
        from issues 
             join reported on reported.issue=issues.number and reported.user='{0}'
       where (select count(*) from assigned where issue=issues.number and user<>'{0}') > 0
    order by issues.category,issues.updated
-    """.format(user), oldIsBad)
+      """},
+}
+
+ksorted = sorted(ops.keys())
+
+index = open('dashboard/index.html', 'w')
+sys.stdout = index
+header('all')
+print("<p>Key:</p><table class='table table-condensed' style='width:auto'>")
+for i, k in enumerate(ksorted):
+   print("<tr><td>%c</td><td>%s</td>" % (chr(ord('A')+i), ops[k]['title']))
+print("</table>")
+
+print("<table class='table sortable table-condensed table-striped table-hover table-responsive' style='width:auto'><thead>")
+print("<tr><th>User</th>")
+for i, k in enumerate(ksorted):
+   print("<th><a href='#' data-toggle='tooltip' title='%s'>%c</a></th>" % (ops[k]['title'], chr(ord('A')+i)))
+print("</tr></thead><tbody>")
+
+for row in cu.execute("select name, lower(name) as lname from users where crl=1 order by lname"):
+    user = row[0]
+    luser = row[1]
+    sys.stdout = open('dashboard/' + user + '.html', 'w')
+    header(user)
+    extra = []
+
+    info = {}
+    for k,v in ops.items():
+       info[k] = genSection(v['title'], k, v['query'].format(user), oldIsBad)
 
     footer()
     sys.stdout.close()
-
+ 
+    sys.stdout = index
+    print("<tr><td><a href='%s.html'>%s</a> <a href='https://github.com/%s'><span class='glyphicon glyphicon-leaf'></span></a>" % (
+       user, luser, user))
+    for i, k in enumerate(ksorted):
+       print("<td><a href='%s.html' data-toggle='tooltip' title='%s'>%d</a></th>" % (user, ops[k]['title'], info[k]))
+    print("</tr>")
+    
 sys.stdout = index
 print("</tbody></table>")
 footer()
