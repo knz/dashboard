f0d799905e14be0df2fe4089fc38c3807da5a4fe
Raphael 'kena' Poss <knz@cockroachlabs.com>
2018-01-19 18:01:24 +0100

    Fix unicode support

--- generate.sh
+++ generate.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 ISSUE=${1:-issue}
+
+unset $(env | grep LC_ | cut -d= -f1)
+# Needed for report.py below.
+export LANG=en_US.UTF-8
+
+set -eui
+
 mkdir -p dashboard
 $ISSUE -json "" >data.json
-python3 json2sql.py <data.json |sqlite3 issues.db
+python3 json2sql.py <data.json >data.sql
+sqlite3 issues.db <data.sql
 python3 report.py
-cp sorttable.js dashboard/
+cp -f sorttable.js dashboard/
